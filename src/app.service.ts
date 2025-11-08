079b2126f4ae75d769c76ef05c8fb98d1e1958c6
VSenichevPersonal <vsenichev@gmail.com>
2025-11-08 21:40:37 +0300

    Fix Prisma import paths to match Docker build context

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from './generated';
+import { PrismaClient } from './src/generated';
 
 @Injectable()
 export class AppService {
